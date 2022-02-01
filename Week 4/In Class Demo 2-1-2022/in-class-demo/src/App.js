3cd4f940ed97c5c1d2f831c1b94fa87d01ebf2ff
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-02-01 16:10:24 -0700

    in class demo

---
+++ Week 4/In Class Demo 2-1-2022/in-class-demo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Body from './Body';
+
+function App() {
+  return (
+    <Header />,
+    <Body />,
+    <Footer />
+  );
+}
+
+export default App;
