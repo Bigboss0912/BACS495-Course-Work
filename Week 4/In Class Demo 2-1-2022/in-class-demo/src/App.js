e00420ea6f073124620b774a217b541291e5b0ca
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-02-01 16:41:30 -0700

    update

--- Week 4/In Class Demo 2-1-2022/in-class-demo/src/App.js
+++ Week 4/In Class Demo 2-1-2022/in-class-demo/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import Body from './Body';
+import Blocks from './Blocks';
 
 function App() {
   return (
-    <Header />,
-    <Body />,
-    <Footer />
+    <div className='App'>
+      <Header />,
+      <Body />,
+      <Blocks />,
+      <Footer />,
+    </div>
   );
 }
 
