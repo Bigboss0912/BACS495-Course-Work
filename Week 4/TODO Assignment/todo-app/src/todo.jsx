d8cd62fca93043f3317d0f609571b0312ec59296
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-02-05 20:00:17 -0700

    create todo app

---
+++ Week 4/TODO Assignment/todo-app/src/todo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function TODO() {
+  
+  const numbers = [];
+  const tasks = [];
+
+  return (
+    <div>
+      <table>
+        <form>
+          for (let index = 0; index < array.length; index++) {
+            
+            
+          }
+        </form>
+      </table>
+    </div>
+  );
+}
+
+export default TODO;
