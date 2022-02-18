2eda82f1494c4229bb2f8f54faaa56a837037df0
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-02-18 04:05:37 -0700

    Update students.js

--- Week 6/Express Assignment/routes/students.js
+++ Week 6/Express Assignment/routes/students.js
@@ -9,20 +9,27 @@ const students = [
   ]
 
 
-router.post('/add/:studentID', function (req, res) {
+router.post('/:studentID', function (req, res) {
   var id = req.params.studentID;
+  
+  var student_ID = {"studentID":id, "firstName":"", "lastName":""};
 
-  students.push(id);
-});
+  students.push(student_ID);
+  console.log(JSON.stringify(student_ID) + " added to students array");
 
-/* GET users listing. */
-router.get('/list', function(req, res) {
   res.send({
     students
   });
 });
 
-router.get('/list/:studentID', function(req, res) {
+/* GET students listing. */
+router.get('/', function(req, res) {
+  res.send({
+    students
+  });
+});
+
+router.get('/:studentID', function(req, res) {
   var id = req.params.studentID;
   
 
@@ -30,6 +37,39 @@ router.get('/list/:studentID', function(req, res) {
     if(student.studentID == id) {
       res.json(student);
     }}
+
+});
+
+router.put('/:studentID/:newFirstName', function(req, res) {
+  var id = req.params.studentID;
+  var name = req.params.newFirstName;
+
+  for (let student of students ) {
+    if(student.studentID == id) {
+      console.log((JSON.stringify(student)) + " changing firstName to " + name);
+      student['firstName'] = name;
+    }}
+
+    res.send({
+      students
+    });
+
+});
+
+router.delete('/:studentID', function(req, res) {
+  var id = req.params.studentID;
+
+  for (let student of students ) {
+    if(student.studentID == id) {
+      console.log((JSON.stringify(student)) + " removing this student");
+      const index = students.indexOf(student);
+      students.splice(index, 1);
+    }
+  }
+  
+  res.send({
+    students
+  });
 
 });
 
